3b3f1c3e86bb03ee7c28c0a64d60af7b9bfef4f3
Atakku <atakkudev@gmail.com>
2021-03-26 02:10:15 +0100

    bruh

--- src/main.rs
+++ src/main.rs
@@ -114,13 +114,23 @@ impl EventHandler for Handler {
             for rea in reacts {
                 ctx.cache.member(&guild, &user).await.expect("Failed to get ,e,ner to add role").add_role(&ctx.http, rea).await.expect("Failed to add role");
             }
-            let mem_roles: Vec<_> = m.roles.iter().filter(|role| OPT_ROLES.contains(role)).collect();
-            for role in mem_roles {
-                ctx.cache.member(&guild, &user).await.expect("Failed to get member to remove role").remove_role(&ctx.http, role).await.expect("Failed to remove role");
-            }
-            r.delete(&ctx.http).await.expect("Failed to delete reaction");
         }
     }
+
+    async fn reaction_remove(&self, ctx: Context, r: Reaction) {
+        if r.user_id.expect("What the fuck") == 810172289462435881 {return;}
+        if r.message_id == 824811302605029397 {
+            let guild = r.guild_id.unwrap();
+            let user = r.user_id.unwrap();
+            let m: Member = ctx.cache.member(&guild, &user).await.unwrap();
+            let reacts: Vec<_> = OPT_REACTS.iter().enumerate().filter(|&(_, reacts)| r.emoji.unicode_eq(reacts)).map(|(i, _)| OPT_ROLES[i]).collect();
+            for rea in reacts {
+                ctx.cache.member(&guild, &user).await.expect("Failed to get ,e,ner to add role").remove_role(&ctx.http, rea).await.expect("Failed to add role");
+            }
+        }
+    }
+
+
 }
 
 
